c76fcc0e16ede7da5867caf9f8cf80a597112e0d
Anton von Schantz <anton.von.schantz@aalto.fi>
2021-04-11 17:38:14 +0300

    Add files via upload

---
+++ expanding_window.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+'''
+Expanding window cross validation
+Similar to sklearn format
+@ germayne  
+'''
+class expanding_window(object):
+    '''	
+    Parameters 
+    ----------
+    
+    Note that if you define a horizon that is too far, then subsequently the split will ignore horizon length 
+    such that there is validation data left. This similar to Prof Rob hyndman's TsCv 
+    
+    
+    initial: int
+        initial train length 
+    horizon: int 
+        forecast horizon (forecast length). Default = 1
+    period: int 
+        length of train data to add each iteration 
+    '''
+    
+
+    def __init__(self,initial= 1,horizon = 1,period = 1):
+        self.initial = initial
+        self.horizon = horizon 
+        self.period = period 
+
+
+    def split(self,data):
+        '''
+        Parameters 
+        ----------
+        
+        Data: Training data 
+        
+        Returns 
+        -------
+        train_index ,test_index: 
+            index for train and valid set similar to sklearn model selection
+        '''
+        self.data = data
+        self.counter = 0 # for us to iterate and track later 
+
+
+        data_length = data.shape[0] # rows 
+        data_index = list(np.arange(data_length))
+         
+        output_train = []
+        output_test = []
+        # append initial 
+        output_train.append(list(np.arange(self.initial)))
+        progress = [x for x in data_index if x not in list(np.arange(self.initial)) ] # indexes left to append to train 
+        output_test.append([x for x in data_index if x not in output_train[self.counter]][:self.horizon] )
+        # clip initial indexes from progress since that is what we are left 
+         
+        while len(progress) != 0:
+            temp = progress[:self.period]
+            to_add = output_train[self.counter] + temp
+            # update the train index 
+            output_train.append(to_add)
+            # increment counter 
+            self.counter +=1 
+            # then we update the test index 
+            
+            to_add_test = [x for x in data_index if x not in output_train[self.counter] ][:self.horizon]
+            output_test.append(to_add_test)
+
+            # update progress 
+            progress = [x for x in data_index if x not in output_train[self.counter]]	
+            
+        # clip the last element of output_train and output_test
+        output_train = output_train[:-1]
+        output_test = output_test[:-1]
+        
+        # mimic sklearn output 
+        index_output = [(train,test) for train,test in zip(output_train,output_test)]
+        
+        return index_output
